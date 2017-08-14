ee22d5f04549a466a9b6714978401f0b281d7476
Acharya <14792481+sai10@users.noreply.github.com>
2017-08-15 05:23:00 +0530

    regex example

---
+++ EmailChecker.java
@@ -0,0 +1,15 @@
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+import java.util.regex.*;
+
+public class TestRegex {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+        Matcher m = p.matcher("XXXX@gmail.com");
+        boolean b = m.matches();
+        if(b == true)
+            System.out.print("Email accepted");
+        else
+            System.out.println("Email not accepted");
+    }
+}
