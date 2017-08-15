7b494029f19ee1489d39fd5979f38ac16764a0be
Acharya <14792481+sai10@users.noreply.github.com>
2017-08-15 05:48:33 +0530

    JSON with Mongo interaction

---
+++ JSONMongo.java
@@ -0,0 +1,52 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.json.JSONObject;
+
+public class TestJsonMongo {
+    public static void main(String[] args) throws Exception{
+
+        JSONObject jo = new JSONObject();
+        String name ;
+        int ph ;
+        String email;
+        String password ;
+        String city ;
+
+
+        jo.put("name","Deepa");
+        jo.put("ph","12345");
+        jo.put("password","qwerty");
+        jo.put("email","deepa@ebullitent.com");
+        jo.put("city","Chennai");
+
+        name = jo.getString("name");
+        ph = Integer.parseInt(jo.getString("ph"));
+        email = jo.getString("email");
+        password = jo.getString("password");
+        city = jo.getString("city");
+
+
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongoClient.getDatabase("test");
+
+        MongoCollection<Document> collection = database.getCollection("user");
+        collection.drop();
+        Document doc = new Document("name", name)
+                .append("ph", ph)
+                .append("email", email)
+                .append("password",password)
+                .append("city", city);
+
+        collection.insertOne(doc);
+
+        MongoCursor<Document> cursor = collection.find().iterator();
+
+        while (cursor.hasNext()) {
+
+            System.out.println(cursor.next());
+        }
+    }
+}
